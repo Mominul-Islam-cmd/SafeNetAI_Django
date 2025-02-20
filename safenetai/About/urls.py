f02149da8e2743d4f311740b03050b0369a7d720
Mominul Islam <mominul@example.com>
2025-02-21 00:33:03 +0600

    update

--- safenetai/About/urls.py
+++ safenetai/About/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from About import views
 
 urlpatterns = [
-    path("", views.about, name='about'),
+    path("about_us/", views.about_us, name='about_us'),
 ]
--- safenetai/About/views.py
+++ safenetai/About/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def about(request):
-    return render(request, "index.html")
+def about_us(request):
+    return render(request, "about_us/about.html")
